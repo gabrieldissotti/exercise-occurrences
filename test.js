1836f168e3bc92912584e1ae34bf150164fbce8f
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-10-26 17:56:11 -0300

    feat(ocurrences in phrase): first commit

---
+++ test.js
@@ -0,0 +1,23 @@
+const phrase = 'aaabbbccc'
+
+function checkPhrase(phrase) {
+
+  const occurrences = [...phrase].reduce((accumulator, letter, index) => {
+    return ({
+      ...accumulator,
+      [letter]: accumulator[letter] ? (accumulator[letter] + 1) : 1
+    })
+  }, {})
+
+  const amount = Object.values(occurrences);
+
+  const data = amount.map((item) => amount.map(subitem =>
+    Boolean(subitem >= (item - 1) && subitem <= (item + 1))
+  ).includes(false));
+
+  return data.includes(false);
+}
+
+console.log(checkPhrase(phrase))
+// solved at 17:52 (26 out.)
+
